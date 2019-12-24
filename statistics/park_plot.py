2c86dd0604ceb0dc47b164fb50fbabe663544bb1
darkMoMo <yx807582@gmail.com>
2019-12-24 21:25:19 +0800

    增加对负一楼车位的判断

--- statistics/park_plot.py
+++ statistics/park_plot.py
@@ -1,5 +1,8 @@
+# encoding:utf-8
 
 negative_two_layers_a = [i for i in range(4, 26)]
+negative_one_layers_a = [i for i in range(9, 40)]
+
 def prepare_data():
     [negative_two_layers_a.append(i) for i in range(28, 31)]
     [negative_two_layers_a.append(i) for i in range(36, 128)]
@@ -25,18 +28,43 @@ def prepare_data():
     negative_two_layers_a.append(401) 
     negative_two_layers_a.append(263) 
 
+    [negative_one_layers_a.append(i) for i in range(61, 88)]
+    [negative_one_layers_a.append(i) for i in range(116, 119)]
+    [negative_one_layers_a.append(i) for i in range(144, 162)]
+    [negative_one_layers_a.append(i) for i in range(183, 185)]
+    [negative_one_layers_a.append(i) for i in range(244, 281)]
+
 def check_plot():
-    park_plot = input("Enter your park plot: [default A plot]")
-    print("Hello, you choose park plot a{}, check result is: ".format(park_plot))
-    if int(park_plot) in negative_two_layers_a:
-        print("It isn't a good park plot!")
+    while True:
+        try:
+            layer = input('请选择楼层: [-1 or -2]')
+            if layer in ['-1', '-2']:
+                break
+        except:
+            print('您输入的楼层必须为 -1 或者 -2！')
+    
+    if layer == '-1':
+        data_layer = negative_one_layers_a
     else:
-        print("It's a good park plot")
+        data_layer = negative_two_layers_a
+
+    park_plot = int(input("请选择车位: "))
+    print("楼层{}，车位 A{}, 检查结果是: ".format(layer, park_plot))
+    
+    if park_plot in data_layer:
+        print("不好的车位!")
+    else:
+        print("好车位")
 
 def main():
-    prepare_data()
-    print(negative_two_layers_a)
-    check_plot()
+    while True:
+        prepare_data()
+        # print("-2 layer：")
+        # print(negative_two_layers_a)
+        # print("----------------------------------------")
+        # print('-1 layer:')
+        # print(negative_one_layers_a)
+        check_plot()
 
 if __name__ == "__main__":
     main()
